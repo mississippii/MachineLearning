3efde36ef9514c993db37b5880c6017b61bc331c
T@nvir <u1604090@student.cuet.ac.bd>
2025-01-25 01:49:19 +0600

    setup repo localmachine

--- SupervisedLearning/SmLinearReg.py
+++ SupervisedLearning/SmLinearReg.py
@@ -6,16 +6,21 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-file_path = "/home/veer/Downloads/Boston.csv"
-data = pd.read_csv(file_path)
-plt.scatter(data['rm'],data['medv'])
+file_path = "/home/veer/Downloads/Dataset/iris/iris.data"
+column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+
+data = pd.read_csv(file_path, header=None, names=column_names)
+
+print(data.head())
+
+plt.scatter(data['sepal_length'],data['sepal_width'])
 plt.xlabel('room Number')
 plt.ylabel('median')
 plt.title('Data set')
 plt.show()
 
-x = data[['rm']]
-y = data[['medv']]
+x = data[['sepal_length']]
+y = data[['sepal_width']]
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -31,8 +36,8 @@ y_pred = model.predict(X_test)
 
 plt.scatter(X_test, y_test, color='blue', label='Actual Data')
 plt.plot(X_test, y_pred, color='red', label='Regression Line')
-plt.xlabel('rm (Number of Rooms)')
-plt.ylabel('medv (Median Value)')
+plt.xlabel('sepal_length (Number of Rooms)')
+plt.ylabel('sepal_width (Median Value)')
 plt.title('Simple Linear Regression')
 plt.legend()
 plt.show()
